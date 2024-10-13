3189bf5bdf6b25a4070e4dd5b3645399de0eea89
Dagonin <dagonins@gmail.com>
2024-10-13 12:14:06 +0200

    input numeru telefonu

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,8 +1,8 @@
 import { AsyncPipe, CommonModule, JsonPipe } from '@angular/common';
-import { Component, forwardRef } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, forwardRef, inject } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButton } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatFormFieldControl, MatFormFieldModule } from '@angular/material/form-field';
 import { MatIcon, MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -10,7 +10,7 @@ import { FormErrorsService } from '../form-errors/form-errors.service';
 import { RegisterService } from './register.service';
 import { PhoneNumberValidator } from '../validators/phone-number-validator';
 import { RouterLink } from '@angular/router';
-import { MyTelInput } from '../phone-input/phone-input.component';
+import { FocusMonitor } from '@angular/cdk/a11y';
 
 
 
@@ -27,15 +27,15 @@ import { MyTelInput } from '../phone-input/phone-input.component';
     MatSidenavModule,
     RouterLink,
     FormsModule,
-    forwardRef(() => MyTelInput),
     AsyncPipe,
     JsonPipe,
     
 ],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+
 
   protected registerForm = new FormGroup({
     login: new FormControl('',[Validators.required, Validators.minLength(5)]),
@@ -44,9 +44,12 @@ export class RegisterComponent {
     password: new FormControl('',[Validators.required,Validators.minLength(8)]),
     passwordRepeat: new FormControl('',[Validators.required,Validators.minLength(8)]),
     area: new FormControl('',[Validators.required,Validators.minLength(2),Validators.maxLength(3)])
-  },{updateOn: 'blur'})
+  })
 
   opened: boolean = false;
+
+  private readonly _focusMonitor = inject(FocusMonitor);
+  private current_val = "";
 
   constructor(
     private registerService: RegisterService,
@@ -69,5 +72,16 @@ export class RegisterComponent {
   maxNumber(len: number){
     this.registerForm
   }
+
+
+
+  autoFocusPrev(control: AbstractControl, prevElement: HTMLInputElement): void {
+    if (control.value.length < 1) {
+      this._focusMonitor.focusVia(prevElement, 'program');
+    }
+  }
+  
+
+
 
 }
